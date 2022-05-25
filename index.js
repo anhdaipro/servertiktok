e90626baa738cfa5929a8e5e25f6af0ddd44e755
phamdai <daipham952@gmail.com>
2022-05-25 20:06:09 +0700

    make it better

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ io.on('connection', socket => {
   })
 })
 
-http.listen(4000, function() {
-  console.log('listening on port 4000')
-})
+app.listen(process.env.PORT || 4000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
