c266e08c7566cad27d0273f002744a938a7361bf
phamdai <daipham952@gmail.com>
2022-05-28 13:21:29 +0700

    make it better

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const socket = require("socket.io");
+const cors = require("cors");
 const io = socket(http, {
   cors: {
       origin: "*",
+      methods:['GET','POST']
   }
 }); 
 const users = {};
-
+app.use(cors());
+const PORT = process.env.PORT || 5000;
 const socketToRoom = {};
 io.on('connection', socket => {
   console.log("New client connected" + socket.id); 
-  
-  socket.on('message', (data) => {
-    io.emit('message', {data})
-  })
- 
+  socket.emit("me", socket.id);
   socket.on("join room", roomID => {
     if (users[roomID]) {
         const length = users[roomID].length;
@@ -29,10 +28,16 @@ io.on('connection', socket => {
     }
     socketToRoom[socket.id] = roomID;
     const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-
     socket.emit("all users", usersInThisRoom);
   });
 
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
   socket.on("sending signal", payload => {
     io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
   });
@@ -46,8 +51,9 @@ io.on('connection', socket => {
     socket.on("disconnect", () => {
       console.log("Client disconnected"); // Khi client disconnect thì log ra terminal.
     });
+    socket.on("message", (data) => {
+      io.emit("message", {data});
+    })
   })
 
-  
-
-http.listen(process.env.PORT || 4000, () => console.log('server is running on port 4000'));
+http.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
