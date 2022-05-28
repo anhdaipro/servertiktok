6e2f71239600920fc1c832dcaa3c40de1ac5d130
phamdai <daipham952@gmail.com>
2022-05-28 22:38:59 +0700

    make it better

--- index.js
+++ index.js
@@ -13,24 +13,17 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 io.on('connection', socket => {
   console.log("New client connected" + socket.id); 
-    socket.on('stream', function(data) {
+    socket.on('stream', data=> {
     // send stream back to room
-      io.emit('stream',{data});
+      socket.broadcast.emit('stream',{data});
   });
   
-  socket.on("sending signal", payload => {
-    io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-  });
-
-  socket.on("returning signal", payload => {
-    io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-  });
+  socket.on("message", (data) => {
+    io.emit("message", {data});
+  })
   socket.on("disconnect", () => {
     console.log("Client disconnected"); // Khi client disconnect thì log ra terminal.
   });
-  socket.on("message", (data) => {
-    io.emit("message", {data});
-    })
-  })
+})
 
 http.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
