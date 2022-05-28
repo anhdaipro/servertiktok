8175f02261c718d4acedcbf55ea53e37aa804a2c
phamdai <daipham952@gmail.com>
2022-05-28 21:33:07 +0700

    make it better

--- index.js
+++ index.js
@@ -15,29 +15,11 @@ const socketToRoom = {};
 io.on('connection', socket => {
   console.log("New client connected" + socket.id); 
   socket.emit("me", socket.id);
-  socket.on("join room", roomID => {
-    if (users[roomID]) {
-        const length = users[roomID].length;
-        if (length === 4) {
-            socket.emit("room full");
-            return;
-        }
-        users[roomID].push(socket.id);
-    } else {
-        users[roomID] = [socket.id];
-    }
-    socketToRoom[socket.id] = roomID;
-    const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-    socket.emit("all users", usersInThisRoom);
+  socket.on('stream', function(data) {
+    // send stream back to room
+    io.broadcast.emit('stream', data);
   });
-
-  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	});
+  
   socket.on("sending signal", payload => {
     io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
   });
@@ -45,12 +27,9 @@ io.on('connection', socket => {
   socket.on("returning signal", payload => {
     io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
   });
-  socket.on("callUser", (data) => {
-    io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
-    })
-    socket.on("disconnect", () => {
-      console.log("Client disconnected"); // Khi client disconnect thì log ra terminal.
-    });
+  socket.on("disconnect", () => {
+    console.log("Client disconnected"); // Khi client disconnect thì log ra terminal.
+  });
     socket.on("message", (data) => {
       io.emit("message", {data});
     })
