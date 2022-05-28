ed70204518b1154d281e64fc0d00e72ed0e0affe
phamdai <daipham952@gmail.com>
2022-05-28 21:43:22 +0700

    make it better

--- index.js
+++ index.js
@@ -8,16 +8,14 @@ const io = socket(http, {
       methods:['GET','POST']
   }
 }); 
-const users = {};
+
 app.use(cors());
 const PORT = process.env.PORT || 5000;
-const socketToRoom = {};
 io.on('connection', socket => {
   console.log("New client connected" + socket.id); 
-  socket.emit("me", socket.id);
-  socket.on('stream', function(data) {
+    socket.on('stream', function(data) {
     // send stream back to room
-    io.broadcast.emit('stream', data);
+      io.emit('stream',{data});
   });
   
   socket.on("sending signal", payload => {
@@ -30,8 +28,8 @@ io.on('connection', socket => {
   socket.on("disconnect", () => {
     console.log("Client disconnected"); // Khi client disconnect thì log ra terminal.
   });
-    socket.on("message", (data) => {
-      io.emit("message", {data});
+  socket.on("message", (data) => {
+    io.emit("message", {data});
     })
   })
 
