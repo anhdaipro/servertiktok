786204c7eb120e1051b5d718c54864fe8b933095
phamdai <daipham952@gmail.com>
2022-05-29 08:01:55 +0700

    make it better

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ io.on('connection', socket => {
   socket.on("sendData", (data) => {
     io.emit("message", {data});
   })
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
   socket.on("disconnect", () => {
     console.log("Client disconnected"); // Khi client disconnect thì log ra terminal.
   });
