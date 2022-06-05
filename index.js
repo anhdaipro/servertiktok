6c4d2d9e6e78563db0eb7ce0864d6d9792a9bd15
phamdai <daipham952@gmail.com>
2022-06-05 07:03:45 +0700

    make it better

--- index.js
+++ index.js
@@ -11,8 +11,35 @@ const io = socket(http, {
 
 app.use(cors());
 const PORT = process.env.PORT || 5000;
-io.on('connection', socket => {
+const users = {};
+
+const socketToRoom = {};
+  io.on('connection', socket => {
   console.log("New client connected" + socket.id); 
+  socket.on("join room", roomID => {
+    if (users[roomID]) {
+        const length = users[roomID].length;
+        if (length === 4) {
+            socket.emit("room full");
+            return;
+        }
+        users[roomID].push(socket.id);
+    } else {
+        users[roomID] = [socket.id];
+    }
+    socketToRoom[socket.id] = roomID;
+    const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+    socket.emit("all users", usersInThisRoom);
+});
+
+socket.on("sending signal", payload => {
+    io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+});
+
+socket.on("returning signal", payload => {
+    io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+});
     socket.on('stream', data=> {
     // send stream back to room
       socket.broadcast.emit('live',{data});
